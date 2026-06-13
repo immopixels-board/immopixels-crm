e8454ccc719d7185164ce460d471a996fa448843
immopixels-board <immopixels@gmail.com>
2026-06-13 14:45:06 +0200

    v4.9.27 demo: Smartlook session-recording (csak demón)

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 import './globals.css'
+import Script from 'next/script'
 export const metadata = { title: 'ImmoPixels CRM', description: 'ImmoPixels Board & CRM' }
 export default function RootLayout({ children }) {
+  const isDemo = process.env.NEXT_PUBLIC_IS_DEMO === '1'
   return (
     <html lang="de">
       <head>
@@ -11,7 +13,19 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="apple-touch-icon" href="/favicon.png" />
       </head>
-      <body>{children}</body>
+      <body>
+        {isDemo && (
+          <Script id="smartlook" strategy="afterInteractive">{`
+            window.smartlook||(function(d) {
+              var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+              var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+              c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+            })(document);
+            smartlook('init', '19b29175d8fd98746b183f875e483784eceb619c', { region: 'eu' });
+          `}</Script>
+        )}
+        {children}
+      </body>
     </html>
   )
 }
